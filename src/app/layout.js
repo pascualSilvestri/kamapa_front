9f7dc9278dee22f48d757afa276ec89be6b5ff1d
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-02-06 11:08:33 -0300

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Loading from './components/Loading'; // Importa el componente de carga
 import SessionAuthProvider from '../../contexts/SessionAuthProvider';
 
+
 export default function RootLayout({ children }) {
 	return (
 		<html lang='es'>
