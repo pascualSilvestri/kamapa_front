03067252701373b6d7c4ee2397252991ebb3aa42
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-02-22 00:07:26 -0300

    agregando las interfaces

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-
+import { User, Rol } from '../../../../types/interfaces';
 import { getSession } from 'next-auth/react';
 import { JWT } from 'next-auth/jwt';
 
