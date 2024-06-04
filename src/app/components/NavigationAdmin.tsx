766bf2953922006a1ce7318e48a5afcde71ef03c
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-06-04 08:17:00 -0300

    Update NavigationAdmin.tsx

--- src/app/components/NavigationAdmin.tsx
+++ src/app/components/NavigationAdmin.tsx
@@ -1,31 +1,28 @@
-'use client'
+'use client';
 import { Nav, Navbar, NavDropdown, Container, Offcanvas } from 'react-bootstrap';
 import Link from 'next/link';
 import Image from 'next/image';
 import ButtonAuth from './ButtonAuth';
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { useInstitucionSelectedContext, useRolesContext, useUserContext } from 'context/userContext';
 
-
-
 export function NavigationAdmin() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
-	const [institucionSelected, setInstitucionSelected] = useInstitucionSelectedContext();
-
-  console.log(session)
+  const [institucionSelected, setInstitucionSelected] = useInstitucionSelectedContext();
 
   useEffect(() => {
     // Si no hay sesión, redirige a la página de inicio de sesión
-    if (!session) {
+    if (status !== 'loading' && !session) {
       router.push('/login');
     }
-      
-    
-  }, []);
+  }, [session, status, router]);
 
+  if (status === 'loading') {
+    return <p>Loading...</p>; // O cualquier componente de carga que prefieras
+  }
 
   return (
     <header data-bs-theme='dark'>
@@ -57,7 +54,13 @@ export function NavigationAdmin() {
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Bienvenido { session.user.nombre} {session.user.apellido}
+                  {session && session.user ? (
+                    <>
+                      Bienvenido {session.user.nombre} {session.user.apellido}
+                    </>
+                  ) : (
+                    'Bienvenido'
+                  )}
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <hr />
@@ -74,74 +77,64 @@ export function NavigationAdmin() {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className='justify-content-end flex-grow-1 pe-3'>
-
-                        <Link style={{
-                          textDecoration: 'none',
-                          color: 'white',
-                          margin: '10px 0',
-                          padding: '10px 20px',
-                          borderRadius: '5px',
-                          backgroundColor: 'transparent',
-                          border: '1px solid white '
-                        }} href='/dashboard'>
-                        Seleccionar Escuela
-                        </Link>
-                        <Link style={{
-                          textDecoration: 'none',
-                          color: 'white',
-                          margin: '10px 0',
-                          padding: '10px 20px',
-                          borderRadius: '5px',
-                          backgroundColor: 'transparent',
-                          border: `1px solid white `
-                        }} href={`/dashboard/Admin/reginstitucion`} >
-                        Registrar una institucion
-                        </Link>
-                        <Link style={{
-                          textDecoration: 'none',
-                          color: 'white',
-                          margin: '10px 0',
-                          padding: '10px 20px',
-                          borderRadius: '5px',
-                          backgroundColor: 'transparent',
-                          border: `1px solid white `
-                        }} href={`/dashboard/Admin/vistainstitucion`} >
-                        Instituciones
-                        </Link>
-                        <Link style={{
-                          textDecoration: 'none',
-                          color: 'white',
-                          margin: '10px 0',
-                          padding: '10px 20px',
-                          borderRadius: '5px',
-                          backgroundColor: 'transparent',
-                          border: `1px solid white `
-                        }} href={`/dashboard/Admin/regAdminUsuario`} >
-                        Registrar usuario
-                        </Link>
-                        <Link style={{
-                          textDecoration: 'none',
-                          color: 'white',
-                          margin: '10px 0',
-                          padding: '10px 20px',
-                          borderRadius: '5px',
-                          backgroundColor: 'transparent',
-                          border: `1px solid white `
-                        }} href={`/dashboard/Admin/consultaUsuario`} >
-                        Consulta Usuario
-                        </Link>
+                  <Link style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    margin: '10px 0',
+                    padding: '10px 20px',
+                    borderRadius: '5px',
+                    backgroundColor: 'transparent',
+                    border: '1px solid white'
+                  }} href='/dashboard'>
+                    Seleccionar Escuela
+                  </Link>
+                  <Link style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    margin: '10px 0',
+                    padding: '10px 20px',
+                    borderRadius: '5px',
+                    backgroundColor: 'transparent',
+                    border: '1px solid white'
+                  }} href='/dashboard/Admin/reginstitucion'>
+                    Registrar una institucion
+                  </Link>
+                  <Link style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    margin: '10px 0',
+                    padding: '10px 20px',
+                    borderRadius: '5px',
+                    backgroundColor: 'transparent',
+                    border: '1px solid white'
+                  }} href='/dashboard/Admin/vistainstitucion'>
+                    Instituciones
+                  </Link>
+                  <Link style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    margin: '10px 0',
+                    padding: '10px 20px',
+                    borderRadius: '5px',
+                    backgroundColor: 'transparent',
+                    border: '1px solid white'
+                  }} href='/dashboard/Admin/regAdminUsuario'>
+                    Registrar usuario
+                  </Link>
+                  <Link style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    margin: '10px 0',
+                    padding: '10px 20px',
+                    borderRadius: '5px',
+                    backgroundColor: 'transparent',
+                    border: '1px solid white'
+                  }} href='/dashboard/Admin/consultaUsuario'>
+                    Consulta Usuario
+                  </Link>
                 </Nav>
                 <hr />
-                {/* <Form className='d-flex'>
-                  <Form.Control
-                    type='search'
-                    placeholder='Search'
-                    className='me-2'
-                    aria-label='Search'
-                  /> */}
-
                 <ButtonAuth />
-                {/* </Form> */}
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
