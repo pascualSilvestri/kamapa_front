76f742dbce4693dfddc46b1334e3455bb8e6188a
Martin Aaron Lora Jorge <chatmartinlorajorge@gmail.com>
2024-07-30 21:59:04 -0300

    prov

--- src/utils/EnviromenManager.ts
+++ src/utils/EnviromenManager.ts
@@ -4,7 +4,7 @@ enum environment {
 }
 
 export class Environment {
-  private static environment: environment = environment.dev;
+  private static environment: environment = environment.prov;
 
   private static getEnvironment(env: environment): string {
     if (env == environment.prov) {
